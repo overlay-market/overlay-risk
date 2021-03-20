3ef6c4f5659c69c7ecc1f5878d327f934aaf39d5
mikeyrf <mikeyrf@users.noreply.github.com>
2021-03-20 17:31:41 -0400

    fix influxdb write

--- scripts/influx_kv1o.py
+++ scripts/influx_kv1o.py
@@ -14,8 +14,8 @@ def get_config() -> tp.Dict:
     return {
         "token": os.getenv('INFLUXDB_TOKEN'),
         "org": os.getenv('INFLUXDB_ORG'),
-        "bucket": os.getenv('INFLUXDB_BUCKET'),
-        "url": os.getenv('INFLUXDB_URL'),
+        "bucket": "ovl_kv1o",
+        "url": "https://us-central1-1.gcp.cloud2.influxdata.com",
     }
 
 
@@ -41,52 +41,53 @@ def quotes() -> tp.List:
             "token_in": "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",  # WETH
             "token_out": "0xdac17f958d2ee523a2206206994597c13d831ec7",  # USDT
             "amount_in": 1e18,
-            "points": 48*30,  # 30 days
-            "window": 2,  # 1 h rolling
+            "points": 24*30,  # 30 days
+            "window": 2,  # 30min window
         }
     ]
 
 
-# Calcs VaR * a^n
+# Calcs Normalized VaR * a^n
+# See: https://oips.overlay.market/notes/note-4
 def calc_vars(mu: float,
-              sigma_sqrd: float,
+              sig_sqrd: float,
               n: int, t: int, alphas: np.ndarray) -> np.ndarray:
-    sigma = np.sqrt(sigma_sqrd)
+    sig = np.sqrt(sig_sqrd)
     q = 1-alphas
-    pow = mu*n*t + sigma*np.sqrt(n*t)*norm.ppf(q)
+    pow = mu*n*t + sig*np.sqrt(n*t)*norm.ppf(q)
     return np.exp(pow) - 1
 
 
 def get_stats(kv1o, q) -> (str, pd.DataFrame):
-    t = kv1o.periodSize()
-    pair = kv1o.pairFor(q["token_in"], q["token_in"])
+    t = kv1o.periodSize() * q["window"]  # 30m * 2 = 1h
+    pair = kv1o.pairFor(q["token_in"], q["token_out"])
     sample = kv1o.sample(
         q["token_in"],
         q["amount_in"],
-        q["token_in"],
+        q["token_out"],
         q["points"],
         q["window"],
     )
-    # TODO: pd data frame ... for mu, ss, VaR
-    mu = float(np.mean([
-        np.log(sample[i]/sample[i-1]) for i in range(1, len(sample), 1)
-    ]) / t)
-    ss = float(np.var([
-        np.log(sample[i]/sample[i-1]) for i in range(1, len(sample), 1)
-    ]) / t)
 
-    # Get the VaRs for 5%, 1%, 0.1% over 1h (T = periodSize, n = # of periodSizes into the future)
-    n = q["window"]*24  # VaRs 1 day into the future
-    alphas = np.array([0.05, 0.01, 0.001])
+    # timestamp: so don't add duplicates
+    timestamp, _, _ = kv1o.lastObservation(pair)
+
+    # mles
+    rs = [np.log(sample[i]/sample[i-1]) for i in range(1, len(sample), 1)]
+    mu = float(np.mean(rs) / t)
+    ss = float(np.var(rs) / t)
+
+    # VaRs for 5%, 1%, 0.1%, 0.01% alphas, 1 day into the future
+    n = 24  # 24 hrs = 1 day
+    alphas = np.array([0.05, 0.01, 0.001, 0.0001])
     vars = calc_vars(mu, ss, n, t, alphas)
-    data = np.concatenate(([mu, ss], vars), axis=None)
-#    print('mu', mu)
-#    print('ss', ss)
-#    print('VaRs', vars)
-#    print('data', data)
-    df = pd.DataFrame(data=data,
-                      columns=['mu', 'sigmaSqrd', 'VaR 5% * a^n',
-                               'Var 1% * a^n', 'Var 0.1% * a^n'])
+    data = np.concatenate(([timestamp, mu, ss], vars), axis=None)
+
+    df = pd.DataFrame(data=data).T
+    df.columns = ['timestamp', 'mu', 'sigSqrd', 'VaR 5% * a^n',
+                  'Var 1% * a^n', 'Var 0.1% * a^n',
+                  'Var 0.01% * a^n']
+
     return pair, df
 
 
@@ -101,7 +102,9 @@ def main():
 
     for q in quotes():
         _, stats = get_stats(KV1O(), q)
-        write_api.write(bucket=config['bucket'], record=stats,
+        write_api.write(bucket=config['bucket'],
+                        org=config['org'],
+                        record=stats,
                         data_frame_measurement_name=q['id'])
 
     client.close()
