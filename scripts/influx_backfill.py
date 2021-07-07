3492499439acaffd9dad4e78d2b4932e39ed61e0
mikeyrf <23126274+mikeyrf@users.noreply.github.com>
2021-07-06 21:18:42 -0400

    clean up backfill script, flake8

--- scripts/influx_backfill.py
+++ scripts/influx_backfill.py
@@ -1,24 +1,8 @@
-import pandas as pd
-import sys
-import numpy as np
-import os
 import json
-import typing as tp
-import logging
-
-from datetime import datetime
-from scipy.stats import norm
-
-from influxdb_client import InfluxDBClient, Point, WritePrecision, WriteOptions
-from influxdb_client.client.write_api import SYNCHRONOUS, PointSettings
-
-from collections import OrderedDict
-from csv import DictReader
-
-import rx
-from rx import operators as ops
-
 import time
+
+from influxdb_client import InfluxDBClient
+from influxdb_client.client.write_api import SYNCHRONOUS, PointSettings
 
 # init parameters
 bucket = "ovl_sushi"
@@ -43,9 +27,10 @@ df.shape
 # df = df[df['_time'] > "2021-06-22T18:50:00Z"]
 
 
-
-df.drop(labels = ['result', 'table', '_start', '_stop', '_measurement', 'influx-sushi', 'type'], inplace = True, axis = 1)
-df = df.pivot(index = ['id', '_time', 'token0_name', 'token1_name'], columns = ['_field'], values = '_value').reset_index()
+df.drop(labels=['result', 'table', '_start', '_stop',
+        '_measurement', 'influx-sushi', 'type'], inplace=True, axis=1)
+df = df.pivot(index=['id', '_time', 'token0_name', 'token1_name'], columns=[
+              '_field'], values='_value').reset_index()
 df = df.rename_axis(None, axis=1).reset_index(drop=True)
 
 df.loc[df.id == 'Sushiswap: WETH / WBTC', 'token0_name'] = 'WBTC'
@@ -74,7 +59,6 @@ token = creds['token']
 file.close()
 
 
-
 stp = 2000
 for i in range(0, df.shape[0], stp):
     with InfluxDBClient(url=url, token=token, org=org) as client:
@@ -85,13 +69,17 @@ for i in range(0, df.shape[0], stp):
         point_settings = PointSettings(**{"type": "metrics-hourly"})
         point_settings.add_default_tag("influx-sushi", "ingest-data-frame")
 
-        write_api = client.write_api(write_options=SYNCHRONOUS, point_settings=point_settings)
+        write_api = client.write_api(
+            write_options=SYNCHRONOUS, point_settings=point_settings)
 
         j = 1
         while j == 1:
             try:
-                write_api.write(bucket=bucket, record=df[i:i+stp-1], data_frame_measurement_name="mem",\
-                    data_frame_tag_columns=['id', 'token0_name', 'token1_name'])
+                write_api.write(bucket=bucket,
+                                record=df[i:i+stp-1],
+                                data_frame_measurement_name="mem",
+                                data_frame_tag_columns=['id', 'token0_name',
+                                                              'token1_name'])
                 j = 0
             except Exception as e:
                 err_cls = e.__class__
@@ -103,7 +91,6 @@ for i in range(0, df.shape[0], stp):
                 '''
                 print(msg)
                 continue
-
 
     print("Wait 15 secs to push data")
     time.sleep(15)
