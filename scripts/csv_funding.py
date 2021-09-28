6fc712545aa21bd414ca0ec88dfe8da4b2b4e523
Michael Feldman <23126274+mikeyrf@users.noreply.github.com>
2021-09-28 17:32:12 -0400

    Add ev and shortfall calcs to funding csv script (#27)
    * add ev and es
    
    * fix params for csv funding

--- scripts/csv_funding.py
+++ scripts/csv_funding.py
@@ -2,6 +2,8 @@ import pystable
 import pandas as pd
 import numpy as np
 
+from scipy import integrate
+
 
 FILENAME = "ethusd_01012020_08232021"
 FILEPATH = f"csv/{FILENAME}.csv"  # datafile
@@ -84,6 +86,49 @@ def nvalue_at_risk(a: float, b: float, mu: float, sigma: float,
     return nvar_long, nvar_short
 
 
+def nexpected_shortfall(a: float, b: float, mu: float, sigma: float,
+                        k_n: float, v: float, g_inv: float, alpha: float,
+                        t: float) -> (float, float, float, float):
+    x = pystable.create(alpha=a, beta=b, mu=mu*t,
+                        sigma=sigma*(t/a)**(1/a), parameterization=1)
+    oi_imb = ((1-2*k_n)**(np.floor(t/v)))
+
+    def integrand(y): return pystable.pdf(x, [y], 1)[0] * np.exp(y)
+
+    # expected shortfall long
+    cdf_x_ginv = pystable.cdf(x, [g_inv], 1)[0]
+    q_min_long = pystable.q(x, [cdf_x_ginv - alpha], 1)[0]
+    integral_long, _ = integrate.quad(integrand, q_min_long, g_inv)
+    nes_long = oi_imb * (integral_long/alpha - 1)
+
+    # expected shortfall short
+    # TODO: do this calculation out properly math style
+    nes_short = 0  # oi_imb * ()
+
+    return nes_long, nes_short, nes_long * alpha, nes_short * alpha
+
+
+def nexpected_value(a: float, b: float, mu: float, sigma: float,
+                    k_n: float, v: float, g_inv: float, cp: float,
+                    t: float) -> (float, float):
+    x = pystable.create(alpha=a, beta=b, mu=mu*t,
+                        sigma=sigma*(t/a)**(1/a), parameterization=1)
+    oi_imb = ((1-2*k_n)**(np.floor(t/v)))
+
+    def integrand(y): return pystable.pdf(x, [y], 1)[0] * np.exp(y)
+
+    # expected value long
+    cdf_x_ginv = pystable.cdf(x, [g_inv], 1)[0]
+    integral_long, _ = integrate.quad(integrand, -np.inf, g_inv)
+    nev_long = oi_imb * (integral_long - cdf_x_ginv + cp*(1-cdf_x_ginv))
+
+    # expected value short
+    # TODO: do this calculation out properly math style
+    nev_short = 0  # oi_imb * ()
+
+    return nev_long, nev_short
+
+
 def main():
     """
     Fits input csv timeseries data with pystable and generates output
@@ -130,22 +175,60 @@ def main():
     df_ks.to_csv(f"csv/metrics/{FILENAME}-ks.csv")
 
     # For different k values at alpha = 0.05 level (diff n calibs),
-    # plot VaR at times into the future
+    # plot VaR and ES at times into the future
     nvars_long = []
     nvars_short = []
+    ness_long = []
+    ness_short = []
+    nevs_long = []
+    nevs_short = []
     for t in TS:
         nvar_t_long = []
         nvar_t_short = []
+
+        ness_t_long = []
+        ness_t_short = []
+
+        nevs_t_long = []
+        nevs_t_short = []
+
         for k_n in df_ks[f"alpha={ALPHA}"]:
+            # normalized value at risk
             nvar_long, nvar_short = nvalue_at_risk(
                 a=dst.contents.alpha, b=dst.contents.beta,
                 mu=dst.contents.mu_1, sigma=dst.contents.sigma,
                 k_n=k_n, v=TC, g_inv=g_inv, alpha=ALPHA, t=t)
             nvar_t_long.append(nvar_long)
             nvar_t_short.append(nvar_short)
+
+            # normalized expected shortfall (conditional & unconditional)
+            nes_long, nes_short, nes_long_uncond, nes_short_uncond = \
+                nexpected_shortfall(
+                    a=dst.contents.alpha, b=dst.contents.beta,
+                    mu=dst.contents.mu_1, sigma=dst.contents.sigma,
+                    k_n=k_n, v=TC, g_inv=g_inv, alpha=ALPHA, t=t)
+            ness_t_long.append(nes_long)
+            ness_t_short.append(nes_short)
+
+            # normalized expected value
+            nev_long, nev_short = \
+                nexpected_value(
+                    a=dst.contents.alpha, b=dst.contents.beta,
+                    mu=dst.contents.mu_1, sigma=dst.contents.sigma,
+                    k_n=k_n, v=TC, g_inv=g_inv, cp=CP, t=t)
+            nevs_t_long.append(nev_long)
+            nevs_t_short.append(nev_short)
+
         nvars_long.append(nvar_t_long)
         nvars_short.append(nvar_t_short)
 
+        ness_long.append(ness_t_long)
+        ness_short.append(ness_t_short)
+
+        nevs_long.append(nevs_t_long)
+        nevs_short.append(nevs_t_short)
+
+    # VaR dataframe to csv
     df_nvars_long = pd.DataFrame(
         data=nvars_long,
         columns=[f"k={k_n}" for k_n in df_ks[f"alpha={ALPHA}"]],
@@ -163,6 +246,44 @@ def main():
     print(f'nvars short (alpha={ALPHA}):', df_nvars_short)
     df_nvars_short.to_csv(
         f"csv/metrics/{FILENAME}-nvars-short-alpha-{ALPHA}.csv")
+
+    # Expected shortfall dataframe to csv
+    df_ness_long = pd.DataFrame(
+        data=ness_long,
+        columns=[f"k={k_n}" for k_n in df_ks[f"alpha={ALPHA}"]],
+        index=[f"t={t}" for t in TS]
+    )
+    df_ness_short = pd.DataFrame(
+        data=ness_short,
+        columns=[f"k={k_n}" for k_n in df_ks[f"alpha={ALPHA}"]],
+        index=[f"t={t}" for t in TS]
+    )
+    print(f'ness long (alpha={ALPHA}):', df_ness_long)
+    df_ness_long.to_csv(
+        f"csv/metrics/{FILENAME}-ness-long-conditional-alpha-{ALPHA}.csv")
+
+    print(f'ness short (alpha={ALPHA}):', df_ness_short)
+    df_ness_short.to_csv(
+        f"csv/metrics/{FILENAME}-ness-short-conditional-alpha-{ALPHA}.csv")
+
+    # Expected value dataframe to csv
+    df_nevs_long = pd.DataFrame(
+        data=nevs_long,
+        columns=[f"k={k_n}" for k_n in df_ks[f"alpha={ALPHA}"]],
+        index=[f"t={t}" for t in TS]
+    )
+    df_nevs_short = pd.DataFrame(
+        data=nevs_short,
+        columns=[f"k={k_n}" for k_n in df_ks[f"alpha={ALPHA}"]],
+        index=[f"t={t}" for t in TS]
+    )
+    print(f'nevs long (alpha={ALPHA}):', df_nevs_long)
+    df_nevs_long.to_csv(
+        f"csv/metrics/{FILENAME}-nevs-long-alpha-{ALPHA}.csv")
+
+    print(f'nevs short (alpha={ALPHA}):', df_ness_short)
+    df_nevs_short.to_csv(
+        f"csv/metrics/{FILENAME}-nevs-short-alpha-{ALPHA}.csv")
 
 
 if __name__ == '__main__':
