f8f6d9d959ae7c29f93e06617732095e856c3199
deepsp94 <anantsp94@gmail.com>
2022-01-20 12:18:05 +0530

    trying to isolate memory issue
    commented out all pystable code

--- scripts/cron/schedule_metrics_univ3.py
+++ scripts/cron/schedule_metrics_univ3.py
@@ -2,8 +2,8 @@ import os
 
 
 def job():
-    print("Running script influx_metrics_univ3.py")
-    os.system("python scripts/influx_metrics_univ3.py")
+    print("Running script influx_metrics_univ3_rv1.py")
+    os.system("python scripts/influx_metrics_univ3_rv1.py")
 
 
 while True:
