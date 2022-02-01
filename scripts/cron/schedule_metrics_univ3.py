a45eca119fb93b2658f744d04cd7c569c0608606
deepsp94 <anantsp94@gmail.com>
2022-02-01 13:54:32 +0530

    parallel tasks in metrics script

--- scripts/cron/schedule_metrics_univ3.py
+++ scripts/cron/schedule_metrics_univ3.py
@@ -2,8 +2,8 @@ import os
 
 
 def job():
-    print("Running script influx_metrics_univ3.py")
-    os.system("python scripts/influx_metrics_univ3.py")
+    print("Running script influx_metrics_univ3_parallel.py")
+    os.system("python scripts/influx_metrics_univ3_parallel.py")
 
 
 while True:
