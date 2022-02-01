3d7ee589ddab8595efe724d98973949d5854ff50
deepsp94 <anantsp94@gmail.com>
2022-02-01 15:37:24 +0530

    Revert "parallel tasks in metrics script"
    This reverts commit a45eca119fb93b2658f744d04cd7c569c0608606.

--- scripts/cron/schedule_metrics_univ3.py
+++ scripts/cron/schedule_metrics_univ3.py
@@ -2,8 +2,8 @@ import os
 
 
 def job():
-    print("Running script influx_metrics_univ3_parallel.py")
-    os.system("python scripts/influx_metrics_univ3_parallel.py")
+    print("Running script influx_metrics_univ3.py")
+    os.system("python scripts/influx_metrics_univ3.py")
 
 
 while True:
