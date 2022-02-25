ee4fa0fbb2a4517aa153d08e4f006c942fdc3611
deepsp94 <anantsp94@gmail.com>
2022-02-25 07:24:21 +0000

    sleep time changed

--- scripts/shell/run/influx_metrics_univ3.sh
+++ scripts/shell/run/influx_metrics_univ3.sh
@@ -3,7 +3,7 @@
 while :
 do
     python ../../influx_metrics_univ3.py &
-    sleep 20
-    echo "KILL NOW"
+    sleep 600
+    echo "KILL AND RESTART influx_metrics_univ3.py NOW"
     kill -KILL `ps -o pid= -C 'python ../../influx_metrics_univ3.py'`
 done
