20eb7d4c8675d86d3c8d583e2973e3c620e568f6
deepsp94 <anantsp94@gmail.com>
2022-10-06 18:19:58 +0530

    Clean up

--- tests/scripts/test_csv_pricedrift.py
+++ tests/scripts/test_csv_pricedrift.py
@@ -6,7 +6,7 @@ from scripts import csv_pricedrift as cpdrift
 import unittest
 
 
-class TestCsvImpact(unittest.TestCase):
+class TestCsvPricedrift(unittest.TestCase):
 
     def get_csv(self, name) -> pd.DataFrame:
         '''
@@ -20,108 +20,6 @@ class TestCsvImpact(unittest.TestCase):
 
         df = pd.read_csv(base, sep=',')
         return df
-
-    def get_stable_params(self, path) -> pd.DataFrame:
-        '''
-        Helper to return dataframe used to init stable params
-        in all functions
-        '''
-        base = os.path.dirname(os.path.abspath(__file__))
-        base = os.path.abspath(os.path.join(base, os.pardir))
-        base = os.path.join(base, 'helpers')
-        base = os.path.join(base, path)
-        base = os.path.join(base, 'get-stable-params.csv')
-
-        df = pd.read_csv(base, sep=',')
-        return df
-
-    def get_alphas(self, path) -> np.ndarray:
-        '''
-        Helper to return dataframe used for uncertainties
-        in all functions
-        '''
-        base = os.path.dirname(os.path.abspath(__file__))
-        base = os.path.abspath(os.path.join(base, os.pardir))
-        base = os.path.join(base, 'helpers')
-        base = os.path.join(base, path)
-        base = os.path.join(base, 'get-alphas.csv')
-
-        df = pd.read_csv(base, sep=',')
-        return df.values[0]
-
-    def get_v(self, path) -> float:
-        '''
-        Helper to return n to use in all calculations
-        '''
-        base = os.path.dirname(os.path.abspath(__file__))
-        base = os.path.abspath(os.path.join(base, os.pardir))
-        base = os.path.join(base, 'helpers')
-        base = os.path.join(base, path)
-        base = os.path.join(base, 'get-vs.csv')
-
-        df = pd.read_csv(base, sep=',')
-        return float(df['v'])
-
-    def get_cp(self, path) -> float:
-        '''
-        Helper to return cp to use in all calculations
-        '''
-        base = os.path.dirname(os.path.abspath(__file__))
-        base = os.path.abspath(os.path.join(base, os.pardir))
-        base = os.path.join(base, 'helpers')
-        base = os.path.join(base, path)
-        base = os.path.join(base, 'get-cps.csv')
-
-        df = pd.read_csv(base, sep=',')
-        return float(df['cp'])
-
-    def get_mu_max_longs(self, path) -> np.ndarray:
-        '''
-        Helper to return expected mu-max longs for stable params
-        and uncertainty files
-        '''
-        base = os.path.dirname(os.path.abspath(__file__))
-        base = os.path.abspath(os.path.join(base, os.pardir))
-        base = os.path.join(base, 'helpers')
-        base = os.path.join(base, path)
-        base = os.path.join(base, 'get-mu-max-longs.csv')
-
-        df = pd.read_csv(base, sep=',')
-        return df.values[0]
-
-    def get_mu_max_shorts(self, path) -> np.ndarray:
-        '''
-        Helper to return expected mu-max shorts for stable params
-        and uncertainty files
-        '''
-        base = os.path.dirname(os.path.abspath(__file__))
-        base = os.path.abspath(os.path.join(base, os.pardir))
-        base = os.path.join(base, 'helpers')
-        base = os.path.join(base, path)
-        base = os.path.join(base, 'get-mu-max-shorts.csv')
-
-        df = pd.read_csv(base, sep=',')
-        return df.values[0]
-
-    def get_mu_maxs(self, path) -> np.ndarray:
-        '''
-        Helper to return expected mu-maxs for stable params
-        and uncertainty files
-        '''
-        base = os.path.dirname(os.path.abspath(__file__))
-        base = os.path.abspath(os.path.join(base, os.pardir))
-        base = os.path.join(base, 'helpers')
-        base = os.path.join(base, path)
-
-        # long
-        base_l = os.path.join(base, 'get-mu-max-longs.csv')
-        df_l = pd.read_csv(base_l, sep=',')
-
-        # short
-        base_s = os.path.join(base, 'get-mu-max-shorts.csv')
-        df_s = pd.read_csv(base_s, sep=',')
-
-        return np.maximum(df_l.values[0], df_s.values[0])
 
     def test_mu_max_long(self):
         df_params = self.get_csv('get-stable-params.csv')
