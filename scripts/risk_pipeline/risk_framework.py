f8c3048ca9c1eccc733bb2a7cc7cd0f9918b32cc
deepsp94 <anantsp94@gmail.com>
2023-02-08 22:12:29 +0530

    Funding visualization

--- scripts/risk_pipeline/risk_framework.py
+++ scripts/risk_pipeline/risk_framework.py
@@ -1,6 +1,8 @@
 import os
 import argparse
+import numpy as np
 import helpers.helpers as helpers
+import helpers.visualizations as vis
 import risk.parameters.csv_funding as funding
 import risk.parameters.csv_impact as impact
 import risk.parameters.csv_liquidations as liq
@@ -42,11 +44,32 @@ def get_params():
 def main(file_name, p, cp, st, lt):
     results_name = file_name.replace('_treated', '')
     helpers.create_dir(results_name)
+
+    # Run risk scripts
     df_ks, df_nvars_long, df_nvars_short, df_ness_long, df_ness_short,\
         df_nevs_long, df_nevs_short = funding.main(file_name, p, cp)
     df_deltas, df_ls = impact.main(file_name, p, cp, st)
     df_mms, df_betas = liq.main(file_name, p)
     df_mus = drift.main(file_name, p, lt)
+
+    # Funding visualizations
+    # Funding % Paid Daily for Various Anchor Times
+    df_ks['Days'] = df_ks.index
+    df_ks['Days'] = df_ks['Days'].apply(
+        lambda x: int(x.replace('n=', ''))/86400)
+    df_ks["Percentage of position paid as funding"] =\
+        df_ks['alpha=0.05'] * 2 * 3600 * 24
+    vis.line_chart(
+        df_ks,
+        "Funding % Paid Daily for Various Anchor Times (alpha = 0.05)",
+        "Daily funding per anchor time",
+        "Days", "Percentage of position paid as funding",
+        helpers.get_results_dir()+results_name
+    )
+
+    # Spread visualizations
+    # Percentage difference between bid and ask
+    
 
 
 if __name__ == '__main__':
