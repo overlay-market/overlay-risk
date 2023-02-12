e54f7b9ed94450ae8dee482fedaa2d54124f6845
deepsp94 <anantsp94@gmail.com>
2023-02-12 18:15:45 +0530

    Delete old/deprecated scripts

--- scripts/risk_pipeline/risk/overlay/pricing.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def bid(min_twap, delta, lmb, vol):
-    return min_twap * np.exp(-delta - (lmb*vol))
-
-
-def ask(min_twap, delta, lmb, vol):
-    return min_twap * np.exp(delta + (lmb*vol))
