d95e19a6b8d284174bf12b1ab426decf0e14fbec
dryad <67245619+dryad@users.noreply.github.com>
2023-02-16 22:02:15 -0700

    bare minimum risk parameter repo, removed most nested complexity

--- scripts/risk_pipeline/data_prep/treat/treatment/missing_value_treatment.py
+++
@@ -1,6 +0,0 @@
-def forward_fill(df, price_col):
-    print(
-        "Warning: Forward filling all null values. "
-        "Check analysis on missing values.")
-    df[price_col] = df[price_col].ffill()
-    return df
