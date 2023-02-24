511592952c55d3e2ced8c56964c47d6eb4c7b00d
deepsp94 <anantsp94@gmail.com>
2023-02-24 16:45:52 +0530

    Use continuation token and make df from response

--- scripts/risk_pipeline/data_prep/get/reservoir_nft_floors.py
+++ scripts/risk_pipeline/data_prep/get/reservoir_nft_floors.py
@@ -1,10 +1,26 @@
 import requests
+import pandas as pd
 
 
 url = "https://api.reservoir.tools/events/collections/floor-ask/v1"
 
-params = {'collection': '0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D'.lower()}
+params = {
+    'collection': '0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D'.lower(),
+    'startTimestamp': 1653042910,
+    'endTimestamp': 1677235800,
+    'limit': 1000
+}
 
 response = requests.get(url, params)
 
-print(response.text)
+response_dicts = [response.json()]
+i = 0
+while response_dicts[i]['continuation']:
+    print(i)
+    params['continuation'] = response_dicts[0]['continuation']
+    response = requests.get(url, params)
+    response_dicts.append(response.json())
+    i += 1
+
+dfs = [pd.json_normalize(d['events']) for d in response_dicts]
+df = pd.concat(dfs, ignore_index=True)
