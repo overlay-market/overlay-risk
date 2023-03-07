8790451d29fcce41400b7b8671289483bd0b7fd9
deepsp94 <anantsp94@gmail.com>
2023-03-07 13:30:59 +0530

    Function for appending CSVs

--- scripts/risk_pipeline/helpers/helpers.py
+++ scripts/risk_pipeline/helpers/helpers.py
@@ -12,6 +12,13 @@ def csv(df, file_w_path):
         df.to_csv(file_w_path)
 
 
+def append_csv(df, file_w_path):
+    if file_exists(file_w_path):
+        df.to_csv(file_w_path, mode='a', header=False)
+    else:
+        print(f"File {file_w_path} doesn't exist")
+
+
 def create_dir(dir_name):
     dir_path = get_results_dir() + dir_name
     if not os.path.exists(dir_path):
