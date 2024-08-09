0f1a88f14ac4f0a63a79c72523a79c25806dc6b9
omar <omarzamil1010@gmail.com>
2024-08-09 17:52:10 +0300

    Added rescalling function

--- params2/funding2.py
+++ params2/funding2.py
@@ -4,26 +4,31 @@ import numpy as np
 from scipy.stats import levy_stable
 
 # Constants
-ALPHAS = np.array([0.01, 0.025, 0.05, 0.075, 0.1])
-NS = 86400 * np.arange(1, 31)  # 1d, 2d, 3d, ...., 31
+NS = 86400 * np.arange(1, 61)  # 1d, 2d, 3d, ...., 31
 TS = 3600 * 12 * np.arange(1, 121)  # 12h, 24h, 36h, ...., 60d
 ALPHA = 0.05
 
 def fit_levy_stable(log_returns):
     return levy_stable.fit(log_returns)
 
+def rescale_params(alpha, beta, mu, sigma, t):
+    """Rescale parameters to per second distribution."""
+    mu_rescaled = mu * t
+    sigma_rescaled = sigma * t**(1/alpha)
+    return alpha, beta, mu_rescaled, sigma_rescaled
+
 def calculate_kl(alpha, beta, mu, sigma, n, alpha_level):
     # Inverse CDF (quantile) for the 1 - alpha level
     F_inv_1_minus_alpha = levy_stable.ppf(1 - alpha_level, alpha, beta, loc=mu, scale=sigma)
     # Calculate kl
-    kl = 1 / (2 * (n / 86400)) * F_inv_1_minus_alpha
+    kl = (1 / (2 * n)) * F_inv_1_minus_alpha
     return kl
 
 def calculate_ks(alpha, beta, mu, sigma, n, alpha_level):
     # Inverse CDF (quantile) for the alpha level
     F_inv_alpha = levy_stable.ppf(alpha_level, alpha, beta, loc=mu, scale=sigma)
     # Calculate ks
-    ks = 1 / (2 * (n / 86400)) * np.log(2 - np.exp(F_inv_alpha))
+    ks = (1 / (2 * n)) * np.log(2 - np.exp(F_inv_alpha))
     return ks
 
 def calibrate_k(alpha, beta, mu, sigma, ns, alpha_level):
@@ -41,7 +46,7 @@ def calibrate_k(alpha, beta, mu, sigma, ns, alpha_level):
 
 def main():
     # Load historical market data from CSV file
-    csv_filename = Path("C:/Users/HP/Desktop/risk/historical_data1.csv")
+    csv_filename = Path(r"C:\Users\HP\Desktop\risk\overlay-risk\ev_index.csv")
     try:
         df = pd.read_csv(csv_filename, index_col='datetime', parse_dates=True)
         print("Data loaded successfully.")
@@ -54,6 +59,10 @@ def main():
         # Fit levy stable distribution
         alpha, beta, mu, sigma = fit_levy_stable(log_returns)
         print(f"Fitted params: alpha: {alpha}, beta: {beta}, mu: {mu}, sigma: {sigma}")
+
+        # Rescale parameters to per second distribution
+        alpha, beta, mu, sigma = rescale_params(alpha, beta, mu, sigma, 1/86400)
+        print(f"Rescaled params: alpha: {alpha}, beta: {beta}, mu: {mu}, sigma: {sigma}")
 
         # Calibrate funding constants (k) to ensure VaR decays to zero
         df_ks = calibrate_k(alpha, beta, mu, sigma, NS, ALPHA)
